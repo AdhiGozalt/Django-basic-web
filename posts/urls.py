61847e5c770487dadd8f4d8fd2e6c7d25c207adf
AdhiGozalt <Muhammadadhigozalt02@gmail.com>
2024-06-24 18:05:35 +0700

    5th Commit

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
+
 urlpatterns = [
     
-    path('',views.posts_list),
+    path('',views.posts_list, name="list"),
+    
+    #slug adalah konverter jalur, untuk memberi tau django bahwa nilai yang ditangkpak harus sesuai dengan format slug
+    #slug: adalah nama parameter yang akan diteruskan ke view, dalam hal ini slug dari UTL akan tersedia dalam view sebagai argumen bernama slug
+    path('<slug:slug>',views.post_page, name="page"),
    
 ]
